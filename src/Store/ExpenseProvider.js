d5b1d87e2547025d7145a4ba7920d2553d5a8b4a
baikarrohit <baikarrohit1999@gmail.com>
2023-09-10 23:23:32 +0530

    Integrating it with firebase

--- src/Store/ExpenseProvider.js
+++ src/Store/ExpenseProvider.js
@@ -1,14 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ExpenseContext from "./exp-contex";
+import axios from "axios";
 
 const ExpeseProvider = (props) => {
   const [items, setItems] = useState([]);
 
-  const addItemHandler = (item) => {
-    console.log(item)
+  const addItemHandler = async (item) => {
+    try {
+      const res = await axios.post(
+        "https://expense-tracker-project-a61f6-default-rtdb.firebaseio.com/expenses.json",
+        {
+          expense: item,
+        }
+      );
+      console.log("cart items saved in backend", res.data);
+    } catch (err) {
+      console.log("Error while saving the cart to backend", err);
+    }
+
     setItems((prevItems) => [...prevItems, item]);
   };
-  
+
+  const fetchItemFromBackend = async () => {
+    try {
+      const res = await axios.get(
+        "https://expense-tracker-project-a61f6-default-rtdb.firebaseio.com/expenses.json"
+      );
+
+      if (res.data) {
+        const fetchedItems = Object.values(res.data).map(
+          (entry) => entry.expense
+        );
+        setItems(fetchedItems);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    fetchItemFromBackend();
+  }, []);
+
   const contextValue = {
     items: items,
     addItem: addItemHandler,
